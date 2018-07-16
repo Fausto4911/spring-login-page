605a02f6830c7cdbada338ee5477a9b19925dd4a
fausto <ftorres@xpelite.com>
2018-07-16 17:00:18 -0400

    delete button in table view working well

--- src/main/java/spring/login/springlogin/controller/Controller.java
+++ src/main/java/spring/login/springlogin/controller/Controller.java
@@ -4,6 +4,7 @@ package spring.login.springlogin.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import spring.login.springlogin.entity.Product;
@@ -31,6 +32,20 @@ public class Controller {
         model.addAttribute("name", post.getName());
         model.addAttribute("quantity", post.getQuantity());
         model.addAttribute("description", post.getDescription());
+        model.addAttribute("products", productService.getProductList());
+        return "inventory";
+    }
+
+    @RequestMapping(value = "/inventory/delete/{id}", method = RequestMethod.GET)
+    public String removeProduct(@PathVariable String id, Model model) {
+        productService.deleteProduct(Long.valueOf(id));
+        model.addAttribute("products", productService.getProductList());
+        return "inventory";
+    }
+
+    @RequestMapping(value = "/inventory/edit/{id}", method = RequestMethod.GET)
+    public String editProduct(@PathVariable String id, Model model) {
+        productService.deleteProduct(Long.valueOf(id));
         model.addAttribute("products", productService.getProductList());
         return "inventory";
     }
